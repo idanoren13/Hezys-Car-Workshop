3c9d50d867058c409a3430e6203b3da70705456c
Idan Oren <idanor@mta.ac.il>
2022-04-30 21:04:31 +0300

    Add project files.

---
+++ Ex03.ConsoleUI/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ex03.ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Ex03.GarageLogic/Wheel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hezy_s_Cars_Workshop
+{
+    class Wheel
+    {
+    }
+}
